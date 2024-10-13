7f4f91735d3fda1fae06ec58d25792a4484e2543
Alexsant123 <83560396+alexsant123@users.noreply.github.com>
2024-10-13 19:26:58 -0300

    add modificacao na classe SecurityConfigurations

--- src/main/java/mentorialoja/security/SecurityConfigurations.java
+++ src/main/java/mentorialoja/security/SecurityConfigurations.java
@@ -2,6 +2,7 @@ package mentorialoja.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,6 +10,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 
 import org.springframework.http.HttpMethod;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 
 import javax.servlet.http.HttpSessionListener;
@@ -18,11 +22,27 @@ import javax.servlet.http.HttpSessionListener;
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class SecurityConfigurations  extends WebSecurityConfigurerAdapter implements HttpSessionListener{
     @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers(HttpMethod.GET, "/salvarAcesso", "/deleteAcesso")
-                .antMatchers(HttpMethod.POST, " /salvarAcesso", "/deleteAcesso");
-        /*Ingnorando URL no momento para nao autenticar*/
+    public void configure(HttpSecurity http) throws Exception {
+
+        http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .disable().authorizeRequests().antMatchers("/").permitAll()
+                .antMatchers("/index").permitAll()
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+
+                /* redireciona ou da um retorno para index quando desloga*/
+                .anyRequest().authenticated().and().logout().logoutSuccessUrl("/index")
+
+                /*mapeia o logout do sistema*/
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+
+                /*Filtra as requisicoes para login de JWT*/
+                .and().addFilterAfter(new JWTLoginFilter("/login", authenticationManager()),
+                        UsernamePasswordAuthenticationFilter.class)
+
+                .addFilterBefore(new JwtApiAutenticacaoFilter(), UsernamePasswordAuthenticationFilter.class);
+
     }
 
 
 }
+
