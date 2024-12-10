a47127aecda6abdd1cf920eb5675afcc695e0be5
alexsant123 <eua2016@hotmail.com>
2024-12-10 20:50:11 -0300

    add

--- src/main/java/mentorialoja/model/ContaPagar.java
+++ src/main/java/mentorialoja/model/ContaPagar.java
@@ -57,6 +57,10 @@ public class ContaPagar implements Serializable {
 		@JoinColumn(name = "pessoa_id", nullable = false,
 		foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fk"))
 		private Pessoa pessoa;
+	@ManyToOne(targetEntity = Pessoa.class)
+	@JoinColumn(name = "empresa_id", nullable = false,
+			foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "empresa_fk"))
+	private Pessoa empresa;
 
 		@ManyToOne(targetEntity = Pessoa.class)
 		@JoinColumn(name = "pessoa_forn_id", nullable = false, 
